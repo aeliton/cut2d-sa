6dd204de5c31a3800864917f587b4a48f3e20095
Thalita <thalita@MacBook-Pro-de-Thalita.local>
2017-06-26 01:19:57 -0300

    WIP merge w pierces

--- guillotine-draw.py
+++ guillotine-draw.py
@@ -11,109 +11,103 @@ import pygame
 import sys
 # from pygame.locals import *
 # end - drawing stuff to be removed
-
 # W, H = 10, 10
 # xs = np.random.random_integers(1, W//3, 30)
 # ys = np.random.random_integers(1, H//3, 30)
 # rects = [(xs[i], ys[i]) for i in range(len(xs))]
 
-W, H, n, a, b, c, d, e = [int(s) for s in input().split(" ")]
 
-rects = []
+class GuilhotinePainter:
 
-for i in range(0, int(n)):
-    w, h, a, b, c = [int(s) for s in input().split(" ")]
-    rects.append((w, h))
+    def __init__(self, instance):
+        self.white = (255, 255, 255)
+        self.black = (0, 0, 0)
+        self.gray = (211, 211, 211)
 
-print(rects)
-print("------------------------")
+        self.cuts = []
 
-g = Guillotine((W, H), rects)
-p = Painter()
-s = SimulatedAnnealing(W, H, rects)
-pieces = s.initial_solution()
-cut = g.cut(pieces)
-# costs, temperatures, cut = SimulatedAnnealing.execute(s, g.cut(), p.update_line)
+        pygame.init()
+        self.font = pygame.font.SysFont('Arial', 11)
+        pygame.display.set_caption('Box Test')
+        self.screen = pygame.display.set_mode((700, 700), 0, 32)
+        self.screen.fill(self.white)
+        pygame.display.update()
 
-# start - drawing stuff to be removed
+        file_lines = [line.rstrip('\n') for line in open(instance)]
+        self.rects = list(map(lambda line: (int(line.split()[0]), int(line.split()[1])), file_lines[1:]))
 
-white = (255, 255, 255)
-black = (0, 0, 0)
-gray = (211, 211, 211)
+        self.W, self.H = self.rects[2][0], self.rects[2][1]
+        self.guilhotine = Guillotine((self.W, self.H), self.rects)
+        self.painter = Painter()
+        self.simulated_annealing = SimulatedAnnealing(self.W, self.H, self.rects)
 
-pygame.init()
-font = pygame.font.SysFont('Arial', 11)
-pygame.display.set_caption('Box Test')
-screen = pygame.display.set_mode((700, 700), 0, 32)
-screen.fill((white))
-pygame.display.update()
+        pieces = self.simulated_annealing.initial_solution()
+        self.cut = self.cut(pieces)
+        self.cuts.append(self.cut)
 
+        print(self.cut)
+        self.drawGuillotine(self.cut)
+        print(self.area(self.cut))
 
-def f(x):
-    return x * 600//((W+H)//2)
+    def cut(self, pieces):
+        return self.guilhotine.cut(pieces)
 
+    def draw(self):
+        cut_pos = 0
 
-def drawRect(x0, y0, x1, y1, text):
-    pygame.draw.rect(screen, (black), (f(x0), f(y0), f(x1), f(y1)), 1)
-    pygame.display.update()
-    screen.blit(font.render(text, True, (255, 0, 0)), (f(x0) + 1, f(y0) + 1))
-    pygame.display.update()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        cut_pos += 1
+                        if cut_pos == len(self.cuts):
+                            self.cut = self.guilhotine.change(self.cut)
+                            self.cuts.append(self.cut)
+                        else:
+                            self.cut = self.cuts[cut_pos]
+                        print("%d %s" % (cut_pos, self.cut))
+                        self.drawGuillotine(self.cut)
+                    elif event.key == pygame.K_LEFT:
+                        cut_pos = cut_pos - 1 if cut_pos > 0 else len(self.cuts) - 1
+                        self.cut = self.cuts[cut_pos]
+                        print("%d %s" % (cut_pos, self.cut))
+                        self.drawGuillotine(self.cut)
 
+    def f(self, x):
+        return x * 600 // ((self.W + self.H) // 2)
 
-def drawGuillotine(g):
-    screen.fill((white))
-    drawRect(0, 0, W, H, '')
-    __drawGuillotine(g)
+    def drawRect(self, x0, y0, x1, y1, text):
+        pygame.draw.rect(self.screen, self.black, (self.f(x0), self.f(y0), self.f(x1), self.f(y1)), 1)
+        pygame.display.update()
+        self.screen.blit(self.font.render(text, True, (255, 0, 0)), (self.f(x0) + 1, self.f(y0) + 1))
+        pygame.display.update()
 
+    def drawGuillotine(self, g):
+        self.screen.fill(self.white)
+        self.__drawGuillotine(g)
 
-def __drawGuillotine(g):
-    if g is not None:
-        x, y, w, h, q, g1, g2 = g
-        for i in range(q):
-            drawRect(i*w + x, y, w, h, str((w, h)))
-        __drawGuillotine(g1)
-        __drawGuillotine(g2)
+    def __drawGuillotine(self, g):
+        self.drawRect(0, 0, self.W, self.H, '')
+        if g is not None:
+            x, y, w, h, q, g1, g2 = g
+            for i in range(q):
+                self.drawRect(i * w + x, y, w, h, str((w, h)))
+            self.__drawGuillotine(g1)
+            self.__drawGuillotine(g2)
 
+    def area(self, cut):
+        if cut is not None:
+            x, y, w, h, q, g1, g2 = cut
+            a = q * w * h
+            if g1 is not None:
+                a = a + self.area(g1)
+            if g2 is not None:
+                a = a + self.area(g2)
+            return a
 
-def area(cut):
-    if cut is not None:
-        x, y, w, h, q, g1, g2 = cut
-        a = q * w * h
-        if g1 is not None:
-            a = a + area(g1)
-        if g2 is not None:
-            a = a + area(g2)
-        return a
+gui = GuilhotinePainter('input/cut1.txt')
+gui.draw()
 
-
-cuts = []
-
-cuts.append(cut)
-cut_pos = 0
-
-print(cut)
-drawGuillotine(cut)
-print(area(cut))
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                cut_pos += 1
-                if cut_pos == len(cuts):
-                    cut = g.change_cut(cut)
-                    cuts.append(cut)
-                else:
-                    cut = cuts[cut_pos]
-                print("%d %s" % (cut_pos, cut))
-                drawGuillotine(cut)
-            elif event.key == pygame.K_LEFT:
-                cut_pos = cut_pos - 1 if cut_pos > 0 else len(cuts) - 1
-                cut = cuts[cut_pos]
-                print("%d %s" % (cut_pos, cut))
-                drawGuillotine(cut)
-
-# end - drawing stuff to be removed

92638abeec2f5c9215904bd74d8a773ed4f3f38d
Aeliton G. Silva <aeliton@gmail.com>
2017-06-26 23:05:16 -0300

    Fix input files with double spaces

--- cut2d.py
+++ cut2d.py
@@ -5,14 +5,17 @@ from simulated_anealing_cut2d import SimulatedAnnealing
 from plotter import Painter
 import numpy as np
 
+W, H, n, a, b, c, d, e = [int(s) for s in input().split(" ")]
 
-xs = np.random.random_integers(1, 10, 10)
-ys = np.random.random_integers(1, 10, 10)
-rects = [(xs[i], ys[i]) for i in range(len(xs))]
+rects = []
 
-g = Guillotine((60, 60), rects)
+for i in range(0, int(n)):
+    w, h, a, b, c = [int(s) for s in input().split(" ")]
+    rects.append((w, h))
+
+g = Guillotine((W, H), rects)
 p = Painter()
-s = SimulatedAnnealing(60, 60, rects)
+s = SimulatedAnnealing(W, H, rects)
 initial = s.initial_solution()
 costs, temperatures, cut = SimulatedAnnealing.execute(s, initial, p.update_line)
 
